00377610c1392ddae2a48bfb43e3ce6a5d757ef4
thienquang <quangthien@outlook.com>
2018-04-10 22:41:04 +0700

    protocol

--- Polymophism.playground/Contents.swift
+++ Polymophism.playground/Contents.swift
@@ -1,6 +1,63 @@
 import Foundation
 
 protocol Shape {
-  var 
+  var area: Double { get }
 }
 
+struct Square: Shape {
+  let side: Double
+  
+  var area: Double {
+    return side * side
+  }
+}
+
+struct Rectangle: Shape {
+  let width: Double
+  let height: Double
+  
+  var area: Double {
+    return width * height
+  }
+}
+
+struct Rhombus: Shape {
+  let d1: Double
+  let d2: Double
+  
+  var area: Double {
+    return (d1 * d2) / 2
+  }
+}
+
+struct Circle: Shape {
+  let radius: Double
+  
+  var area: Double {
+    return .pi * radius * radius
+  }
+}
+
+var shape: Shape
+shape = Rectangle(width: 10, height: 20)
+print(shape.area)
+
+shape = Circle(radius: 100)
+print(shape.area)
+
+shape = Rhombus(d1: 20, d2: 50)
+print(shape.area)
+
+var shapes = [Shape]()
+let square = Square(side: 15)
+shapes.append(square)
+
+let rectangle = Rectangle(width: 10, height: 20)
+shapes.append(rectangle)
+
+let circle = Circle(radius: 100)
+shapes.append(circle)
+
+for shape in shapes {
+  print(shape.area)
+}
